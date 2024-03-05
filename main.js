7d7c8b73f7cb039ea8218bf3882ac23b0b18a02f
k1ego <ladonin05@mail.ru>
2024-03-05 21:31:08 +0300

    Made switching between pages without transferring content

--- main.js
+++ main.js
@@ -1,49 +1,113 @@
-// const goods = document.querySelector('button[data-id="goods"]')
-// const cart = document.querySelector('button[data-id="cart"]');
+let activeTabId = 'goods'
 
-// console.log(goods, cart)
+const initialTab = getActiveTab()
 
-// function clickHandler(event) {
-//     const id = event.target.dataset.id;
+initialTab.classList.add('active')
 
-//     if (id === "goods"){
-//         goods.classList.add("active")
-//         cart.classList.remove("active")
-//     }
+renderTabContentById(activeTabId);
 
-//     else if (id === "cart"){
-//         cart.classList.add("active")
-//         goods.classList.remove("active")
-//     }
-// }
+// ---
 
-// goods.addEventListener("click", clickHandler)
-// cart.addEventListener("click", clickHandler)
-
-let activeTabId = "goods";
-
-const initialTab = document.querySelector(
-	`button[data-tab-id = "${activeTabId}"]`
-);
-
-initialTab.classList.add('active');
-
-// --
-
-const tabs = document.querySelectorAll('button.tab');
+const tabs = document.querySelectorAll('button.tab')
 
 for (let i = 0; i < tabs.length; i++) {
-	const tab = tabs[i];
-	tab.addEventListener('click', clickHandler);
+	const tab = tabs[i]
+
+	tab.addEventListener('click', clickHandler)
 }
 
 function clickHandler(event) {
-	const activeTab = document.querySelector(
-		`button[data-tab-id = "${activeTabId}"]`
-	);
-    
-    activeTab.classList.remove("active");
-    event.target.classList.add("active");
+	const activeTab = getActiveTab()
 
-    activeTabId = event.target.dataset.tabId;
+	activeTab.classList.remove('active')
+	event.target.classList.add('active')
+
+	activeTabId = event.target.dataset.tabId
+
+	removeActiveTabContent()
+
+	renderTabContentById(activeTabId);
+}
+
+function getActiveTab() {
+	return document.querySelector(`button[data-tab-id="${activeTabId}"]`)
+}
+
+function removeActiveTabContent(){
+    const activeContent = document.querySelector(
+			`[data-active-tab-content="true"]`
+		);
+
+		activeContent.remove();
+}
+
+
+function renderTabContentById(tabId) {
+    const tabsContainer = document.querySelector('.tabs')
+    let html = '';
+	if (tabId === 'goods') {
+		html = renderGoods();
+	} else {
+		html = renderCart();
+	}
+    tabsContainer.insertAdjacentHTML('afterend', html)
+}
+
+// рендерим товары
+function renderGoods() {
+	return `
+    <div data-active-tab-content="true" class="product-items">
+        <div class="product-item">
+            <img src="sources/images/bmw1.jpg">
+            <div class="product-list">
+                <h3>BMW m8</h3>
+                <p class="price">₽ 300</p>
+                <button class="button">В корзину</button>
+            </div>
+        </div>
+
+        <div class="product-item">
+            <img src="sources/images/bmw2.jpg">
+            <div class="product-list">
+                <h3>BMW m3</h3>
+                <p class="price">₽ 150</p>
+                <button class="button">В корзину</button>
+            </div>
+        </div>
+
+        <div class="product-item">
+            <img src="sources/images/bmw3.jpg">
+            <div class="product-list">
+                <h3>BMW m5</h3>
+                <p class="price">₽ 260</p>
+                <button class="button">В корзину</button>
+            </div>
+        </div>
+    </div>
+	`
+}
+
+// рендерим корзину
+function renderCart() {
+	return `
+    <div data-active-tab-content="true" class="cart-items">
+        <div class="cart-item">
+            <div class="cart-item-title">BMW m8</div>
+            <div class="cart-item-count">3шт.</div>
+            <div class="cart-item-price">₽ 150</div>
+        </div>
+
+        <div class="cart-item">
+            <div class="cart-item-title">BMW m3</div>
+            <div class="cart-item-count">1шт.</div>
+            <div class="cart-item-price">₽ 450</div>
+        </div>
+
+        <div class="cart-item">
+            <div class="cart-item-title">BMW m5</div>
+            <div class="cart-item-count">6шт.</div>
+            <div class="cart-item-price">₽ 550</div>
+        </div>
+    </div>
+	`
 }
