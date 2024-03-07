801535c7bb32f1bb1ac8bbc0062d22be6f561505
k1ego <ladonin05@mail.ru>
2024-03-07 22:40:33 +0300

    Added removing items from cart

--- goods.js
+++ goods.js
@@ -1,17 +1,20 @@
 const GOODS = [
 	{
+		id: 1,
 		name: 'BMW M8',
 		price: '18кк',
 		imgSrc: 'sources/images/bmw1.jpg',
 	},
 	{
+		id: 2,
 		name: 'BMW M3',
 		price: '10кк',
 		imgSrc: 'sources/images/bmw2.jpg',
 	},
 	{
+		id: 3,
 		name: 'BMW M5',
 		price: '15кк',
 		imgSrc: 'sources/images/bmw3.jpg',
 	},
-]
+];
